f5410d24d9b1574a2d23749ce433dba43bf7d078
mhhong <mh.hong@proton.me>
2025-05-12 15:50:54 +0900

    Weather condition to trigger an alert has been modified -- <800

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ from datetime import datetime
 if open_weather_api.check_weather():
     kakao_api.send_message()
     print("Message has been sent successfully.")
+else:
+    print("It's not supposed to be raining for the next 12 hours.")
 
 
 
--- open_weather_api.py
+++ open_weather_api.py
@@ -6,28 +6,26 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #------------------------------------------------ OPEN WEATHER API ------------------------------------------------#
-API_KEY = os.getenv("API_KEY")
+WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
 OWM_Endpoint = os.getenv("OWM_Endpoint")
 
 weather_params = {
     "lat": "37.609892",
     "lon": "126.731146",
-    "appid": API_KEY,
+    "appid": WEATHER_API_KEY,
     "cnt": 5,
 }
 
 def check_weather():
     weather_response = requests.get(OWM_Endpoint, params=weather_params)
-    # print(response.status_code)
     weather_response.raise_for_status()
     weather_data = weather_response.json()
+    print(weather_data)
 
     for hour_data in weather_data["list"]:
         condition_code = hour_data["weather"][0]["id"]
 
-        if condition_code <= 700:
-            return True
-        else:
-            return False
+        return True if condition_code < 700 else False
 
+print(check_weather())
 
